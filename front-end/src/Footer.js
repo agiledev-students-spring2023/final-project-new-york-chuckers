5400e28d0e60ccd14a717ee7eab220bdd1ce2699
les5185 <70672721+les5185@users.noreply.github.com>
2023-03-19 00:01:15 -0400

    add eslint prettier and reformat files

--- front-end/src/Footer.js
+++ front-end/src/Footer.js
@@ -6,15 +6,15 @@ import './Footer.css';
 function Footer() {
   return (
     <div>
-        <div className="footer-section">
-      <div className="footer-content">
-        <div className="footer-left">
-          <span className="terms">Terms and Conditions</span>
+      <div className="footer-section">
+        <div className="footer-content">
+          <div className="footer-left">
+            <span className="terms">Terms and Conditions</span>
+          </div>
+          <div className="footer-right">
+            <span className="email">Email Us: contact@testerconnector.com</span>
+          </div>
         </div>
-        <div className="footer-right">
-          <span className="email">Email Us: contact@testerconnector.com</span>
-        </div>
-      </div>
       </div>
     </div>
   );
--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -7,14 +7,16 @@ function Header() {
   return (
     <div className="header-bar">
       <div className="logo-container">
-        <img src={logo} alt="Tester Logo" className="logo"/>
+        <img src={logo} alt="Tester Logo" className="logo" />
       </div>
       <div className="search-container">
         <input type="text" placeholder="Search..." />
         <button type="submit">Search</button>
       </div>
       <div className="login-container">
-        <Link to="/login" className="router-link">Login</Link>
+        <Link to="/login" className="router-link">
+          Login
+        </Link>
       </div>
     </div>
   );
