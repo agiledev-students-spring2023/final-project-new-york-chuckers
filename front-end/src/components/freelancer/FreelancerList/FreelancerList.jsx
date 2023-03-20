9080f5b8d1164467f7c8e1e3899fd91c45b1bcac
les5185 <70672721+les5185@users.noreply.github.com>
2023-03-20 11:02:42 -0400

    modify freelancer list

--- front-end/src/components/freelancer/FreelancerList/FreelancerList.jsx
+++ front-end/src/components/freelancer/FreelancerList/FreelancerList.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { freelancerApi } from '../../../api/freelancer';
 import { List } from '../../common/list/List';
 import { ListItem } from '../../common/list/ListItem';
 import FreelancerListItem from '../FreelancerListItem/FreelancerListItem';
 import './FreelancerList.css';
 
 function FreelancerList() {
+  const [freelancers, setFreelancers] = useState([]);
+
+  useEffect(() => {
+    const fetchFreelancers = async () => {
+      const data = await freelancerApi.listFreelancers();
+
+      setFreelancers(data);
+    };
+
+    fetchFreelancers();
+  }, []);
+
   return (
     <div className="freelancer-list__wrapper">
-      <List>{/* TODO: Add multiple FreelancerListItem components*/}</List>
+      <List>
+        {freelancers.map(({ id, name, position, pay }) => (
+          <FreelancerListItem
+            key={id}
+            name={name}
+            position={position}
+            pay={pay}
+          />
+        ))}
+      </List>
     </div>
   );
 }
--- front-end/src/components/freelancer/FreelancerListItem/FreelancerListItem.jsx
+++ front-end/src/components/freelancer/FreelancerListItem/FreelancerListItem.jsx
@@ -3,20 +3,16 @@ import { Button } from '../../common/Button';
 import { ListItem } from '../../common/list/ListItem';
 import './FreelancerListItem.css';
 
-function FreelancerListItem({ name, position, hoursPay }) {
+function FreelancerListItem({ name, position, pay }) {
   return (
     <ListItem style={{ backgroundColor: '#F3F6F8' }}>
       <div className="freelancer-list-item__wrapper">
         <div className="freelancer-list-item__content">
-          <div className="freelancer-list-item__content-name">
-            Freelancer Name: {name}
-          </div>
+          <div className="freelancer-list-item__content-name">Name: {name}</div>
           <div className="freelancer-list-item__content-info">
-            Freelancer Position: {position}
+            Position: {position}
           </div>
-          <div className="freelancer-list-item__content-info">
-            Hours Pay: ${hoursPay}
-          </div>
+          <div className="freelancer-list-item__content-info">Pay: {pay}</div>
         </div>
         <Button>View Profile</Button>
       </div>
