7c20c9ce156b1432d07dd2beceb3182b86d95492
Omri Sokol <71156698+omri-sokol@users.noreply.github.com>
2023-03-22 11:18:11 -0400

    Update FreelancerListPage.jsx

--- front-end/src/pages/FreelancerListPage/FreelancerListPage.jsx
+++ front-end/src/pages/FreelancerListPage/FreelancerListPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Header } from '../../components/common/Header';
 import { FreelancerList } from '../../components/freelancer/FreelancerList';
 import { SearchBar } from '../../components/common/SearchBar';
+import { HomeButton } from '../../components/common/HomeButton';
 import './FreelancerListPage.css';
 
 function FreelancerListPage() {
@@ -22,6 +23,7 @@ function FreelancerListPage() {
           <li key={index}>{result}</li>
         ))}
       </ul>
+      <HomeButton />
     </div>
   );
 
