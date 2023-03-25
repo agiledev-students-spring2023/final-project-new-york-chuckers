1094195b1a5ed0849f40c8f884d2a4533b1efab3
les5185 <tiffanylee3123@gmail.com>
2023-03-24 22:19:21 -0400

    add link in logo

--- front-end/src/components/common/Logo/Logo.jsx
+++ front-end/src/components/common/Logo/Logo.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../../Assets/Logo.png';
 import './Logo.css';
 
 function Logo() {
   return (
-    <div className="logo__wrapper">
-      <img src={logo} />
-    </div>
+    <Link to="/">
+      <div className="logo__wrapper">
+        <img src={logo} />
+      </div>
+    </Link>
   );
 }
 
