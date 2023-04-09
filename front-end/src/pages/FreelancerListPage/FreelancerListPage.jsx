1bff9b5fa8a3c434bc311fcc065338163fa4ba43
les5185 <tiffanylee3123@gmail.com>
2023-04-08 23:20:41 -0400

    remove unused input, button in freelancer list

--- front-end/src/pages/FreelancerListPage/FreelancerListPage.jsx
+++ front-end/src/pages/FreelancerListPage/FreelancerListPage.jsx
@@ -1,32 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Header } from '../../components/common/Header';
 import { FreelancerList } from '../../components/freelancer/FreelancerList';
-import { SearchBar } from '../../components/common/SearchBar';
-import { HomeButton } from '../../components/common/HomeButton';
 import './FreelancerListPage.css';
 
 function FreelancerListPage() {
-  const [results, setResults] = useState([]);
-
-  const handleSearch = query => {
-    // Perform search logic here and update the results state
-    setResults([...results, query]);
-  };
-
   return (
     <div className="freelancer-list-page__wrapper">
       <Header />
       <FreelancerList />
-      <SearchBar onSearch={handleSearch} />
-      <ul>
-        {results.map((result, index) => (
-          <li key={index}>{result}</li>
-        ))}
-      </ul>
-      <HomeButton />
     </div>
   );
-
 }
 
 export default FreelancerListPage;
