c6c0f52f43265a826eb5e0ef5cc33dc39087b607
les5185 <tiffanylee3123@gmail.com>
2023-04-08 23:52:51 -0400

    add searchbar in freelancer list

--- front-end/src/components/freelancer/FreelancerList/FreelancerList.jsx
+++ front-end/src/components/freelancer/FreelancerList/FreelancerList.jsx
@@ -4,11 +4,13 @@ import { useState } from 'react';
 import { freelancerApi } from '../../../api/freelancer';
 import { List } from '../../common/list/List';
 import { ListItem } from '../../common/list/ListItem';
+import { SearchBar } from '../../common/SearchBar';
 import FreelancerListItem from '../FreelancerListItem/FreelancerListItem';
 import './FreelancerList.css';
 
 function FreelancerList() {
   const [freelancers, setFreelancers] = useState([]);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchFreelancers = async () => {
@@ -20,10 +22,21 @@ function FreelancerList() {
     fetchFreelancers();
   }, []);
 
+  function handleSearch(query) {
+    setQuery(query);
+  }
+
+  const filteredFreelancers = freelancers.filter(
+    p =>
+      p.name.toLowerCase().includes(query.toLowerCase()) ||
+      p.position.toLowerCase().includes(query.toLowerCase()),
+  );
+
   return (
     <div className="freelancer-list__wrapper">
+      <SearchBar onSearch={handleSearch} />
       <List>
-        {freelancers.map(({ id, name, position, pay }) => (
+        {filteredFreelancers.map(({ id, name, position, pay }) => (
           <FreelancerListItem
             key={id}
             id={id}
