fa7664a962035434f888767c25281062b3a4eec0
les5185 <tiffanylee3123@gmail.com>
2023-04-08 23:52:58 -0400

    add searchbar in position list

--- front-end/src/components/position/PositionList/PositionList.jsx
+++ front-end/src/components/position/PositionList/PositionList.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { List } from '../../common/list/List';
 import { PositionListItem } from '../PositionListItem';
 import { positionApi } from '../../../api/position';
 import Button from '../../common/Button/Button';
+import { SearchBar } from '../../common/SearchBar';
 import './PositionList.css';
 
 function PositionList() {
   const [positions, setPositions] = useState([]);
+  const [query, setQuery] = useState('');
 
-  //create the navigate variable for the link to create new position 
+  //create the navigate variable for the link to create new position
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -23,20 +25,31 @@ function PositionList() {
   }, []);
 
   //function to handle if new position is clicked
-  function handleNew(){
-    navigate('/new-position')
+  function handleNew() {
+    navigate('/new-position');
   }
+
+  function handleSearch(query) {
+    setQuery(query);
+  }
+
+  const filteredPositions = positions.filter(
+    p =>
+      p.name.toLowerCase().includes(query.toLowerCase()) ||
+      p.position.toLowerCase().includes(query.toLowerCase()),
+  );
 
   return (
     <div className="position-list__wrapper">
       {
         //here I added the create new position button
       }
-      <div className='open-button__wrapper'>
+      <SearchBar onSearch={handleSearch} />
+      <div className="open-button__wrapper">
         <Button onClick={handleNew}>New Position</Button>
       </div>
       <List>
-        {positions.map(({ id, name, position, pay }) => (
+        {filteredPositions.map(({ id, name, position, pay }) => (
           <PositionListItem
             key={id}
             id={id}
