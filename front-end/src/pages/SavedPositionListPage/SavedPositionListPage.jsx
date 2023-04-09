1c23191e037fc8c5f79f80bfe053845c6d8b5377
les5185 <tiffanylee3123@gmail.com>
2023-04-08 23:21:03 -0400

    remove unused input, button in saved position list

--- front-end/src/pages/SavedPositionListPage/SavedPositionListPage.jsx
+++ front-end/src/pages/SavedPositionListPage/SavedPositionListPage.jsx
@@ -1,28 +1,13 @@
 import React, { useState } from 'react';
 import { Header } from '../../components/common/Header';
 import { PositionList } from '../../components/position/PositionList';
-import { SearchBar } from '../../components/common/SearchBar';
-import { HomeButton } from '../../components/common/HomeButton';
 import './SavedPositionListPage.css';
 
 function SavedPositionListPage() {
-  const [results, setResults] = useState([]);
-
-  const handleSearch = query => {
-    setResults([...results, query]);
-  };
-
   return (
     <div className="saved-position-list-page__wrapper">
       <Header />
       <PositionList />
-      <SearchBar onSearch={handleSearch} />
-      <ul>
-        {results.map((result, index) => (
-          <li key={index}>{result}</li>
-        ))}
-      </ul>
-      <HomeButton />
     </div>
   );
 }
