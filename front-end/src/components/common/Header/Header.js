88de4e3ad1324fe5aadbb5ea354dc376883e3dd8
les5185 <tiffanylee3123@gmail.com>
2023-04-08 23:52:42 -0400

    remove search bar in header

--- front-end/src/components/common/Header/Header.js
+++ front-end/src/components/common/Header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
-import SearchBar from '../SearchBar/SearchBar';
 import { UserLoginButton } from '../UserLoginButton';
 import './Header.css';
 
@@ -8,7 +7,6 @@ function Header() {
   return (
     <div className="header-bar">
       <Logo />
-      <SearchBar />
       <UserLoginButton />
     </div>
   );
