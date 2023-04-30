9b17984aac28fec2dd5f4956e58fc45ee1d28242
les5185 <tiffanylee3123@gmail.com>
2023-04-29 23:41:17 -0400

    add getLoginUserType function

--- front-end/src/utils/parseToken.js
+++ front-end/src/utils/parseToken.js
@@ -2,5 +2,14 @@ export const parseJwtPayload = token => JSON.parse(atob(token.split('.')[1]));
 
 export const getLoginUserId = () => {
   const token = localStorage.getItem('id');
-  return parseJwtPayload(token).id;
+  if (token !== null) {
+    return parseJwtPayload(token).id;
+  }
+};
+
+export const getLoginUserType = () => {
+  const token = localStorage.getItem('id');
+  if (token !== null) {
+    return parseJwtPayload(token).type;
+  }
 };
