2674b66a2f96b114b08450dc37443f80f411c122
les5185 <tiffanylee3123@gmail.com>
2023-05-03 00:34:42 -0400

    update validation

--- back-end/validators/user/register.js
+++ back-end/validators/user/register.js
@@ -4,16 +4,11 @@ const isEmpty = require("is-empty");
 const validateRegisterInput = (data) => {
   let errors = {};
 
-  data.name = !isEmpty(data.name) ? data.name : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   data.confirmPassword = !isEmpty(data.confirmPassword)
     ? data.confirmPassword
     : "";
-
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
-  }
 
   if (Validator.isEmpty(data.email)) {
     errors.email = " Email field is required";
@@ -29,9 +24,6 @@ const validateRegisterInput = (data) => {
     errors.confirmPassword = "Confirm password field is required";
   }
 
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be at least 6 characters";
-  }
   if (!Validator.equals(data.password, data.confirmPassword)) {
     errors.confirmPassword = "Passwords must match";
   }
