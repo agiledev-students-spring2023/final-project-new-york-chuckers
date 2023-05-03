52c4b40ac25ff370e2ee255d62769f8c430ccd8b
Omri Sokol <71156698+omri-sokol@users.noreply.github.com>
2023-05-03 11:07:04 -0400

    Update HamburgerBar.jsx

--- front-end/src/components/common/HamburgerBar/HamburgerBar.jsx
+++ front-end/src/components/common/HamburgerBar/HamburgerBar.jsx
@@ -1,2 +1,36 @@
+import './HamburgerBar.css';
 import React, { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { useEffect } from 'react';
+
+// functionality
+function HamburgerBar({ onSearch }) {
+  const [query, setQuery] = useState('');
+
+  // handling change
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+
+  // On click
+  useEffect(() => {
+    onSearch(query);
+  }, [query]);
+
+  // Specfic Actions
+  return (
+    <div className="hamburger-bar__wrapper">
+      <form className="hamburger-bar_form">
+        <input
+          type="text"
+          value={query}
+          onChange={handleInputChange}
+          className="Hamburger-bar__input"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default HamburgerBar;
 
